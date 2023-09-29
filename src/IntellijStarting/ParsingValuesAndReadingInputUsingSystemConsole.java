d96d057e2c6e4754a1e2975fe646a70dc6d879af
shreyas129 <shreyas.tathode129@gmail.com>
2023-09-29 18:26:39 +0530

    commit

---
+++ src/IntellijStarting/ParsingValuesAndReadingInputUsingSystemConsole.java
@@ -0,0 +1,10 @@
+package IntellijStarting;
+
+public class ParsingValuesAndReadingInputUsingSystemConsole {
+    public static void main(String[] args) {
+        int currentYear = 2023;
+        String usersDateOfBirthOfBirth = "2014";
+
+        System.out.println("My age = " + (currentYear + usersDateOfBirthOfBirth));
+    }
+}
