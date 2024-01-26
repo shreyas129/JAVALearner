d43832e502c96b41e69149302d25a8d6b1556e96
shreyas129 <shreyas.tathode129@gmail.com>
2024-01-26 21:48:25 +0530

    commit

---
+++ src/IntellijStarting/test/SimpleCalculator.java
@@ -0,0 +1,54 @@
+package IntellijStarting.test;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+    }
+
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0;
+        } else {
+            return firstNumber / secondNumber;
+        }
+    }
+}
