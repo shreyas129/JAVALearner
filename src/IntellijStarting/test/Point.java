e82a9f9857dd3dd80e347272cd446809f7465c44
shreyas129 <shreyas.tathode129@gmail.com>
2024-02-01 16:11:19 +0530

    commit

--- src/IntellijStarting/test/Point.java
+++ src/IntellijStarting/test/Point.java
@@ -4,6 +4,16 @@ public class Point {
     private int x;
     private int y;
 
+    public static void main(String[] args) {
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+    }
+
     public Point() {}
 
     public Point(int x, int y) {
--- src/IntellijStarting/test/Wall.java
+++ src/IntellijStarting/test/Wall.java
@@ -4,6 +4,15 @@ public class Wall {
     private double width;
     private double height;
 
+    public static void main(String[] args) {
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
+
     public Wall() {
     }
 
