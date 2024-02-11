d4e81d4e9f5d7a91b92c0f3682a6ab54a2e5b66a
shreyas129 <shreyas.tathode129@gmail.com>
2024-02-11 11:28:02 +0530

    commit

---
+++ src/IntellijStarting/test/Carpet.java
@@ -0,0 +1,17 @@
+package IntellijStarting.test;
+
+public class Carpet {
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = cost;
+
+        if (cost < 0) {
+            this.cost = 0;
+        }
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
