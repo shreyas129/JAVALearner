dafa37ffa09e2913f283d3d1a4f5b00ae4bec483
shreyas129 <shreyas.tathode129@gmail.com>
2024-02-14 22:11:42 +0530

    commit

---
+++ src/IntellijStarting/test/Main92InheritanceChallenge1.java
@@ -0,0 +1,14 @@
+package IntellijStarting.test;
+
+public class Main92InheritanceChallenge1 {
+    public static void main(String[] args) {
+        Employee prashant = new Employee("Prashant", "4/01/1980", "7/05/2022");
+        System.out.println(prashant);
+        System.out.println("Age = " + prashant.getAge());
+        System.out.println("Pay = " + prashant.collectPay());
+
+        Employee joe = new Employee("Joe", "11/11/1990", "03/03/2020");
+        System.out.println(joe);
+        
+    }
+}
---
+++ src/IntellijStarting/test/Worker.java
@@ -0,0 +1,40 @@
+package IntellijStarting.test;
+
+import java.time.LocalDate;
+
+public class Worker {
+    private String name;
+    private String birthDate;
+    protected String endDate;
+
+    public Worker() {
+
+    }
+
+    public Worker(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public int getAge() {
+        int currentYear = LocalDate.now().getYear();
+        int birthYear = Integer.parseInt(birthDate.substring(6));
+        return currentYear - birthYear - 1000;
+    }
+
+    public double collectPay() {
+        return 0.0;
+    }
+    public void terminate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+}
