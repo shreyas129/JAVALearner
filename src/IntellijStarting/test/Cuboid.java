e8fa6bddd7f3ad823d31af518f6da781b17958cd
shreyas129 <shreyas.tathode129@gmail.com>
2024-03-17 23:03:14 +0530

    commit

---
+++ src/IntellijStarting/test/Cuboid.java
@@ -0,0 +1,18 @@
+package IntellijStarting.test;
+
+public class Cuboid extends Main37Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return height * getArea();
+    }
+}
---
+++ src/IntellijStarting/test/Main37Rectangle.java
@@ -0,0 +1,34 @@
+package IntellijStarting.test;
+
+public class Main37Rectangle {
+    private double width, length;
+    public static void main(String[] args) {
+        Main37Rectangle rectangle = new Main37Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
+    }
+
+    public Main37Rectangle(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return length * width;
+    }
+}
