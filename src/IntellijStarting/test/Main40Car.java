71ab7de4c6de252293d821ac6708c4d771663a57
shreyas129 <shreyas.tathode129@gmail.com>
2024-03-23 14:52:10 +0530

    commit

---
+++ src/IntellijStarting/test/Main40Car.java
@@ -0,0 +1,117 @@
+package IntellijStarting.test;
+
+public class Main40Car {
+    private boolean engine = true;
+    private int cylinders, wheels = 4;
+    private String name;
+
+    public Main40Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Main40Car car = new Main40Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+    }
+
+    public String startEngine() {
+        return this.getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    public String accelerate() {
+        return this.getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    public String brake() {
+        return this.getClass().getSimpleName() + " -> brake()";
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Mitsubishi extends Main40Car {
+    public Mitsubishi(int cylinder, String name) {
+        super(cylinder, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return super.startEngine();
+    }
+
+    @Override
+    public String accelerate() {
+        return super.accelerate();
+    }
+
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+}
+
+class Holden extends Main40Car {
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return super.startEngine();
+    }
+
+    @Override
+    public String accelerate() {
+        return super.accelerate();
+    }
+
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+}
+
+class Ford extends Main40Car {
+    public Ford(int cylinder, String name) {
+        super(cylinder, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return super.startEngine();
+    }
+
+    @Override
+    public String accelerate() {
+        return super.accelerate();
+    }
+
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+}
