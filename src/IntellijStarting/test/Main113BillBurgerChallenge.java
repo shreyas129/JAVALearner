d741cf7d9768b172b4b8fe250e765640384853ec
shreyas129 <shreyas.tathode129@gmail.com>
2024-03-25 19:22:27 +0530

    commit

--- src/IntellijStarting/test/Main113BillBurgerChallenge.java
+++ src/IntellijStarting/test/Main113BillBurgerChallenge.java
@@ -2,22 +2,22 @@ package IntellijStarting.test;
 
 public class Main113BillBurgerChallenge {
     public static void main(String[] args) {
-//        Item coke = new Item("drink", "coke", 1.50);
-//        coke.printItem();
-//        coke.setSize("LARGE");
-//        coke.printItem();
-//
-//        Item avocado = new Item("Topping", "avocado", 1.50);
-//        avocado.printItem();
-//
-//        Burger burger = new Burger("regular", 4.00);
-//        burger.addToppings("BACON", "Cheese", "MAYO");
-//        burger.printItem();
+        Item coke = new Item("drink", "coke", 1.50);
+        coke.printItem();
+        coke.setSize("LARGE");
+        coke.printItem();
 
-//        MealOrder regularMeal = new MealOrder();
-//        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
-//        regularMeal.setDrinkSize("LARGE");
-//        regularMeal.printItemizedList();
+        Item avocado = new Item("Topping", "avocado", 1.50);
+        avocado.printItem();
+
+        Burger burger = new Burger("regular", 4.00);
+        burger.addToppings("BACON", "Cheese", "MAYO");
+        burger.printItem();
+
+        MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        regularMeal.setDrinkSize("LARGE");
+        regularMeal.printItemizedList();
 
         MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
         secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
