46bca74bebc2e4e0ffab54b3ca55c850672b522c
shreyas129 <shreyas.tathode129@gmail.com>
2024-03-30 19:31:03 +0530

    commit

--- src/IntellijStarting/Main120UsingArrays.java
+++ src/IntellijStarting/Main120UsingArrays.java
@@ -7,7 +7,7 @@ public class Main120UsingArrays {
     public static void main(String[] args) {
         int[] firstArray = getRandomArray(10);
         System.out.println(Arrays.toString(firstArray));
-        Arrays.sort(firstArray);
+         Arrays.sort(firstArray);
         System.out.println(Arrays.toString(firstArray));
 
         int[] secondArray = new int[10];
@@ -30,6 +30,22 @@ public class Main120UsingArrays {
 
         int[] largerArray = Arrays.copyOf(thirdArray, 15);
         System.out.println(Arrays.toString(largerArray));
+
+        String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+        if(Arrays.binarySearch(sArray, "Mark") >= 0) {
+            System.out.println("Found Mark in the list");
+        }
+        
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {1, 2, 3, 4, 5, 0};
+
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("Arrays are equal");
+        } else {
+            System.out.println("Arrays are not equal");
+        }
     }
 
     private static int[] getRandomArray(int len) {
@@ -37,7 +53,7 @@ public class Main120UsingArrays {
         int[] newInt = new int[len];
 
         for (int i = 0; i < len; i++) {
-            newInt[1] = random.nextInt(100);
+            newInt[i] = random.nextInt(100);
         }
 
         return newInt;
