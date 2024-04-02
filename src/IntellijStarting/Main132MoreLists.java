197837b6aa0edc1022e204ff5d3a5a0976261ca6
Shreyas129 <shreyas.tathode129@gmail.com>
2024-04-02 19:06:51 +0530

    commit

---
+++ src/IntellijStarting/Main132MoreLists.java
@@ -0,0 +1,23 @@
+package IntellijStarting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main132MoreLists {
+    public static void main(String[] args) {
+        String[] items = {"apples", "bananas", "milk", "eggs"};
+
+        List<String> list = List.of(items);
+        System.out.println(list);
+
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yoghurt");
+        System.out.println(groceries);
+
+        ArrayList<String> nextList = new ArrayList<>(List.of("pickles", "mustard", "cheese"));
+        System.out.println(nextList);
+
+        groceries.addAll(nextList);
+        System.out.println(groceries);
+    }
+}
