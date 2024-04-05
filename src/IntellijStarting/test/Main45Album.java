d1d30ad0d3b6bec505017e746395f2990e0746ad
Shreyas129 <shreyas.tathode129@gmail.com>
2024-04-05 23:35:52 +0530

    commit

---
+++ src/IntellijStarting/test/Main45Album.java
@@ -0,0 +1,91 @@
+package IntellijStarting.test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main45Album {
+
+    private String name, artist;
+    private ArrayList<Song> songs;
+
+    public static void main(String[] args) {
+        ArrayList<Main45Album> albums = new ArrayList<>();
+
+        Main45Album album = new Main45Album("Stormbringer", "Deep Purple");
+        album.addSong("Stormbringer", 4.6);
+        album.addSong("Love don't mean a thing", 4.22);
+        album.addSong("Holy man", 4.3);
+        album.addSong("Hold on", 5.6);
+        album.addSong("Lady double dealer", 3.21);
+        album.addSong("You can't do it right", 6.23);
+        album.addSong("High ball shooter", 4.27);
+        album.addSong("The gypsy", 4.2);
+        album.addSong("Soldier of fortune", 3.13);
+        albums.add(album);
+
+        album = new Main45Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        album.addSong("I put the finger on you", 3.25);
+        album.addSong("Lets go", 3.45);
+        album.addSong("Inject the venom", 3.33);
+        album.addSong("Snowballed", 4.51);
+        album.addSong("Evil walks", 3.45);
+        album.addSong("C.O.D.", 5.25);
+        album.addSong("Breaking the rules", 5.32);
+        album.addSong("Night of the long knives", 5.12);
+        albums.add(album);
+
+        LinkedList<Song> playList = new LinkedList<>();
+        albums.get(0).addToPlayList("You can't do it right", playList);
+        albums.get(0).addToPlayList("Holy man", playList);
+        albums.get(0).addToPlayList("Speed king", playList);  // Does not exist
+        albums.get(0).addToPlayList(9, playList);
+        albums.get(1).addToPlayList(3, playList);
+        albums.get(1).addToPlayList(2, playList);
+        albums.get(1).addToPlayList(24, playList);  // There is no track 24
+    }
+
+    public Main45Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public boolean addSong(String title, double duration) {
+
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for (Song checkedSong : songs) {
+            if (checkedSong.getTitle().equals(title)) {
+                return checkedSong;
+            }
+        }
+        return null;
+    }
+    
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+
+        int index = trackNumber - 1;
+        if ((index >= 0) && (index <= songs.size())) {
+            playList.add(songs.get(index));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+
+        Song checkedSong = findSong(title);
+        if (checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        return false;
+    }
+}
