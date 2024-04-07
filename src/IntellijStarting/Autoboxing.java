81abfee7be9444783242010acd304fac7257e576
Shreyas129 <shreyas.tathode129@gmail.com>
2024-04-07 14:27:05 +0530

    commit

--- src/IntellijStarting/Autoboxing.java
+++ src/IntellijStarting/Autoboxing.java
@@ -1,5 +1,9 @@
 package IntellijStarting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Autoboxing {
     public static void main(String[] args) {
         Integer boxedInt = Integer.valueOf(15);      // preferred but unnecessary
@@ -14,6 +18,33 @@ public class Autoboxing {
 
         Double resultBoxed = getLiteralDoublePrimitive();
         double resultUnboxed = getDoubleObject();
+
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 50;
+        System.out.println(Arrays.toString(wrapperArray));
+        System.out.println(wrapperArray[0].getClass().getName());
+
+        Character[] characterArray = {'a', 'b', 'c', 'd'};
+        System.out.println(Arrays.toString(characterArray));
+
+        var ourList = List.of(1, 2, 3, 4, 5);
+        System.out.println(ourList);
+    }
+
+    private static ArrayList<Integer> getList(Integer... varargs) {
+        ArrayList<Integer> aList = new ArrayList<>();
+        for (int i : varargs) {
+            aList.add(i);
+        }
+        return aList;
+    }
+
+    private static int returnAnInt(Integer i) {
+        return i;
+    }
+
+    private static Integer returnAnInteger(int i) {
+        return i;
     }
 
     private static double getDoubleObject() {
