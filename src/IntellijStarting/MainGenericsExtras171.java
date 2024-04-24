b20f78150bcddc2ef351942d9283e74ca5e48756
Shreyas129 <shreyas.tathode129@gmail.com>
2024-04-24 20:15:29 +0530

    commit

--- src/IntellijStarting/MainGenericsExtras171.java
+++ src/IntellijStarting/MainGenericsExtras171.java
@@ -11,19 +11,53 @@ public class MainGenericsExtras171 {
             students.add(new Student171());
         }
         students.add(new LPAStudent171());
-        printList(students);
+//        printList(students);
+        printMoreLists(students);
 
         List<LPAStudent171>LPAStudents=new ArrayList<>();
         for(int i=0;i<studentCount;i++){
             LPAStudents.add(new LPAStudent171());
         }
-        printList(LPAStudents);
+//        printList(LPAStudents);
+        printMoreLists(LPAStudents);
+
+        testList(new ArrayList<String>(List.of("Able", "Barry", "Charlie")));
+        testList(new ArrayList<Integer>(List.of(1, 2, 3)));
     }
 
-    public static void printList(List students){
+    public static void printMoreLists(List<? extends Student171> students){
         for(var student:students){
             System.out.println(student);
         }
         System.out.println();
     }
+
+//    public static <T extends Student171> void printList(List<T> students){
+//        for(var student:students){
+//            System.out.println(student.getYearStarted()+": "+student);
+//        }
+//        System.out.println();
+//    }
+
+    public static void testList(List<?>list){
+        for(var element:list){
+            if(element instanceof String s){
+                System.out.println("String: "+s.toUpperCase());
+            } else if(element instanceof Integer i) {
+                System.out.println("Integer: " +i.floatValue());
+            }
+        }
+    }
+
+//    public static void testList(List<String> list) {
+//        for(var element:list) {
+//            System.out.println("String: "+element.toUpperCase());
+//        }
+//    }
+//
+//    public static void testList(List<Integer> list) {
+//        for(var element:list) {
+//            System.out.println("Integer: "+element.floatValue());
+//        }
+//    }
 }
