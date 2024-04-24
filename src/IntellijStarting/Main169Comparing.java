6808087e742b3c9092706e4b17fb5d924b45911e
Shreyas129 <shreyas.tathode129@gmail.com>
2024-04-24 18:16:19 +0530

    commit

--- src/IntellijStarting/Main169Comparing.java
+++ src/IntellijStarting/Main169Comparing.java
@@ -1,6 +1,8 @@
 package IntellijStarting;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 
 public class Main169Comparing {
     public static void main(String[] args) {
@@ -32,25 +34,47 @@ public class Main169Comparing {
         Arrays.sort(students);
         System.out.println(Arrays.toString(students));
 
-        System.out.println("result = "+shreyas.compareTo("Mary"));
+        System.out.println("result = "+shreyas.compareTo(new Student169("SHREYAS")));
+
+        Comparator<Student169> gpaSorter = new StudentGPAComparator170();
+        Arrays.sort(students, gpaSorter.reversed());
+        System.out.println(Arrays.toString(students));
     }
 }
 
-class Student169 implements Comparable{
-    private String name;
+class StudentGPAComparator170 implements Comparator<Student169>{
+    @Override
+    public int compare(Student169 o1, Student169 o2) {
+        return (o1.gpa+o1.name).compareTo(o2.gpa+ o2.name);
+    }
+}
+
+class Student169 implements Comparable<Student169>{
+    private static int LAST_ID = 1000;
+    private static Random random = new Random();
+    String name;
+    private int id;
+    protected double gpa;
 
     public Student169(String name) {
         this.name = name;
+        id = LAST_ID++;
+        gpa = random.nextDouble(1.0, 4.0);
     }
 
     @Override
     public String toString() {
-        return name;
+        return "%d - %s (%.2f)".formatted(id, name, gpa);
     }
 
+//    @Override
+//    public int compareTo(Object o) {
+//        Student169 other = (Student169) o;
+//        return name.compareTo(other.name);
+//    }
+
     @Override
-    public int compareTo(Object o) {
-        Student169 other = (Student169) o;
-        return name.compareTo(other.name);
+    public int compareTo(Student169 o) {
+        return Integer.valueOf(id).compareTo(Integer.valueOf(o.id));
     }
 }
