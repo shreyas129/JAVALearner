d61416e625e7b665c8aff8c22e1569d234217a05
Shreyas129 <shreyas.tathode129@gmail.com>
2024-05-05 12:41:46 +0530

    lambda expressions 3

--- src/IntellijStarting/Main185LambdaExpressions.java
+++ src/IntellijStarting/Main185LambdaExpressions.java
@@ -3,14 +3,16 @@ package IntellijStarting;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
 
 public class Main185LambdaExpressions {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(List.of("alpha", "bravo", "charlie", "delta"));
 
-        for(String s : list){
+        for (String s : list) {
             System.out.println(s);
         }
 
@@ -21,7 +23,7 @@ public class Main185LambdaExpressions {
         String prefix = "nato";
         list.forEach((var myString) -> {
             char first = myString.charAt(0);
-            System.out.println(prefix + " " + myString + " means " +first);
+            System.out.println(prefix + " " + myString + " means " + first);
         });
 
         int result = calculator((var a, var b) -> a + b, 5, 2);
@@ -55,15 +57,47 @@ public class Main185LambdaExpressions {
         System.out.println("-------");
         list.removeIf(s -> s.startsWith("ea"));
         list.forEach(s -> System.out.println(s));
+
+        list.replaceAll(s -> s.charAt(0) + " - " + s.toUpperCase());
+        System.out.println("-------");
+        list.forEach(s -> System.out.println(s));
+
+        String[] emptyStrings = new String[10];
+        System.out.println(Arrays.toString(emptyStrings));
+        Arrays.fill(emptyStrings, "");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> (i + 1) + ". ");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> (i + 1) + ". " + switch (i) {
+            case 0 -> "one";
+            case 1 -> "two";
+            case 2 -> "three";
+            default -> "";
+        });
+        System.out.println(Arrays.toString(emptyStrings));
+
+        String[] names = {"Ann", "Bob", "Carol", "David", "Ed", "Fred"};
+        String[] randomList = randomlySelectedValues(15, names, () -> new Random().nextInt(0, names.length));
+        System.out.println(Arrays.toString(randomList));
     }
 
     public static <T> T calculator(BinaryOperator<T> function, T value1, T value2) {
         T result = function.apply(value1, value2);
-        System.out.println("Result of operation: "+result);
+        System.out.println("Result of operation: " + result);
         return result;
     }
 
     public static <T> void processPoint(T t1, T t2, BiConsumer<T, T> consumer) {
         consumer.accept(t1, t2);
+    }
+
+    public static String[] randomlySelectedValues(int count, String[] values, Supplier<Integer> s) {
+        String[] selectedValues = new String[count];
+        for(int i = 0; i < count; i++) {
+            selectedValues[i] = values[s.get()];
+        }
+        return selectedValues;
     }
 }
