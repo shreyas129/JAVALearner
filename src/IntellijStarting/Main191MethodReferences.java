94f29f798948198cdfdcac47d791d5651dcf1b72
Shreyas129 <shreyas.tathode129@gmail.com>
2024-05-08 16:40:41 +0530

    Method References

---
+++ src/IntellijStarting/Main191MethodReferences.java
@@ -0,0 +1,42 @@
+package IntellijStarting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+
+class PlainOld191 {
+    private static int last_id = 1;
+    private int id;
+
+    public PlainOld191() {
+        id = PlainOld191.last_id++;
+        System.out.println("Creating a PlainOld object: id = " + id);
+    }
+}
+
+public class Main191MethodReferences {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("Anna", "Bob", "Chuck" ,"Dave"));
+        list.forEach(System.out::println);
+
+        calculator(Integer::sum, 10, 25);
+        calculator(Double::sum, 2.5, 7.5);
+
+        Supplier<PlainOld191> reference1 = PlainOld191::new;
+        PlainOld191 newPojo = reference1.get();
+        System.out.println("Getting array");
+        PlainOld191[] pojo1 = seedArray(PlainOld191::new, 10);
+    }
+
+    private static <T> void calculator(BinaryOperator<T> function, T value1, T value2) {
+        System.out.println("Result of operation: " + function.apply(value1, value2));
+    }
+
+    private static PlainOld191[] seedArray(Supplier<PlainOld191> reference, int count) {
+        PlainOld191[] array = new PlainOld191[count];
+        Arrays.setAll(array, i -> reference.get());
+        return array;
+    }
+}
