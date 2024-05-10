dc709d060ef8f7a06d0ecdb9408a88a4e0bf7b95
Shreyas129 <shreyas.tathode129@gmail.com>
2024-05-10 20:12:23 +0530

    Most Confusing Method references

--- src/IntellijStarting/Main191MethodReferences.java
+++ src/IntellijStarting/Main191MethodReferences.java
@@ -3,8 +3,7 @@ package IntellijStarting;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 class PlainOld191 {
     private static int last_id = 1;
@@ -28,6 +27,27 @@ public class Main191MethodReferences {
         PlainOld191 newPojo = reference1.get();
         System.out.println("Getting array");
         PlainOld191[] pojo1 = seedArray(PlainOld191::new, 10);
+
+        calculator((s1, s2) -> s1.concat(s2), "Hello ", "World");
+        calculator(String::concat, "Hello ", "World");
+
+        BinaryOperator<String> b1 = String::concat;
+        BiFunction<String, String, String> b2 = String::concat;
+        UnaryOperator<String> u1 = String::toUpperCase;
+
+        System.out.println(b1.apply("Hello ", "World"));
+        System.out.println(b2.apply("Hello ", "World"));
+        System.out.println(u1.apply("Hello "));
+
+        String result = "Hello".transform(u1);
+        System.out.println("Result = " + result);
+
+        result = result.transform(String::toLowerCase);
+        System.out.println("Result = " + result);
+
+        Function<String, Boolean> f0 = String::isEmpty;
+        boolean resultBoolean = result.transform(f0);
+        System.out.println("Result = " + resultBoolean  );
     }
 
     private static <T> void calculator(BinaryOperator<T> function, T value1, T value2) {
