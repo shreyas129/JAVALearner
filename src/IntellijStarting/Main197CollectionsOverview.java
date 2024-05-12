48bc0464f57d417b9d3978c62f8f777e261f8cdc
Shreyas129 <shreyas.tathode129@gmail.com>
2024-05-12 15:52:47 +0530

    Collections overview

---
+++ src/IntellijStarting/Main197CollectionsOverview.java
@@ -0,0 +1,23 @@
+package IntellijStarting;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Main197CollectionsOverview {
+    public static void main(String[] args) {
+        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
+        Collection<String> list = new HashSet<>(Arrays.asList(names));
+        System.out.println(list);
+
+        list.add("Fred");
+        list.addAll(Arrays.asList("George", "Gary", "Grace"));
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+        list.sort();
+    }
+}
