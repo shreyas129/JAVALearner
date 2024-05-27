5a2948e42eebc2f657852fc42f9f64e2700fcdf3
Shreyas129 <shreyas.tathode129@gmail.com>
2024-05-27 17:54:13 +0530

    Collections part 2

--- src/IntellijStarting/test/Main198DeckOfCardsChallenge.java
+++ src/IntellijStarting/test/Main198DeckOfCardsChallenge.java
@@ -4,9 +4,6 @@ import java.util.*;
 
 public class Main198DeckOfCardsChallenge {
     public static void main(String[] args) {
-        List<Card198> deck = Card198.getStandardDeck();
-        Card198.printDeck(deck);
-
         Card198[] cardArray = new Card198[13];
         Card198 aceOfHearts = Card198.getFaceCard(Card198.Suit198.HEART, 'A');
         Arrays.fill(cardArray, aceOfHearts);
@@ -24,7 +21,6 @@ public class Main198DeckOfCardsChallenge {
         List<Card198> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
         Card198.printDeck(kingsOfClubs, "Kings of clubs", 1);
 
-
         Collections.addAll(cards, cardArray);
         Card198.printDeck(cards, "Card collection with aces added", 1);
 
@@ -34,5 +30,37 @@ public class Main198DeckOfCardsChallenge {
 
         cards = List.copyOf(kingsOfClubs);
         Card198.printDeck(cards, "List Copy of Kings", 1);
+
+        List<Card198> deck = Card198.getStandardDeck();
+        Card198.printDeck(deck);
+
+        Collections.shuffle(deck);
+        Card198.printDeck(deck, "Shuflled deck", 4);
+
+        Collections.reverse(deck);
+        Card198.printDeck(deck, "reversed deck of cards", 4);
+
+        var sortingAlgorithm = Comparator.comparing(Card198::rank).thenComparing(Card198::suit);
+        Collections.sort(deck, sortingAlgorithm);
+        Card198.printDeck(deck, "Standard deck sorted by rank, suit", 13);
+
+        Collections.reverse(deck);
+        Card198.printDeck(deck, "Sorted by rank, suit reversed:", 13);
+
+        List<Card198> kings = new ArrayList<>(deck.subList(4, 8));
+        Card198.printDeck(kings, "Kings in deck", 1);
+
+        List<Card198> tens = new ArrayList<>(deck.subList(16, 20));
+        Card198.printDeck(tens, "Tens in deck", 1);
+
+        int subListIndex = Collections.indexOfSubList(deck, tens);
+        System.out.println("sublist index for tens = " + subListIndex);
+        System.out.println("Contains = " + deck.containsAll(tens));
+
+        boolean disjoint = Collections.disjoint(deck, tens);
+        System.out.println("disjoint = " + disjoint);
+
+        boolean disjoint2 = Collections.disjoint(kings, tens);
+        System.out.println("disjoint = " + disjoint2);
     }
 }
