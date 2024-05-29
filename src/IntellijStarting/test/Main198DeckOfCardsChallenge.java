47271cccd88637ff8e8ac15d352189dcc7768907
Shreyas129 <shreyas.tathode129@gmail.com>
2024-05-29 13:35:10 +0530

    Collections part 3

--- src/IntellijStarting/test/Main198DeckOfCardsChallenge.java
+++ src/IntellijStarting/test/Main198DeckOfCardsChallenge.java
@@ -12,7 +12,7 @@ public class Main198DeckOfCardsChallenge {
         List<Card198> cards = new ArrayList<>(52);
         Collections.fill(cards, aceOfHearts);
         System.out.println(cards);
-        System.out.println("cards.size()" + cards.size());
+        System.out.println("cards.size() = " + cards.size());
 
         List<Card198> acesOfHearts = Collections.nCopies(13, aceOfHearts);
         Card198.printDeck(acesOfHearts, "Aces of Hearts", 1);
@@ -62,5 +62,54 @@ public class Main198DeckOfCardsChallenge {
 
         boolean disjoint2 = Collections.disjoint(kings, tens);
         System.out.println("disjoint = " + disjoint2);
+
+        deck.sort(sortingAlgorithm);
+        Card198 tenOfHearts = Card198.getNumericCard(Card198.Suit198.HEART, 10);
+        int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
+        System.out.println("foundIndex = " + foundIndex);
+        System.out.println("foundIndex = " + deck.indexOf(tenOfHearts));
+        System.out.println(deck.get(foundIndex));
+
+        Card198 tenOfClubs = Card198.getNumericCard(Card198.Suit198.CLUB, 10);
+        Collections.replaceAll(deck, tenOfClubs, tenOfHearts);
+        Card198.printDeck(deck.subList(32, 36), "Tens list", 1);
+
+        Collections.replaceAll(deck, tenOfHearts, tenOfClubs);
+        Card198.printDeck(deck.subList(32, 36), "Tens list", 1);
+
+        if (Collections.replaceAll(deck, tenOfHearts, tenOfClubs)) {
+            System.out.println("Tens Of hearts replaced with tens of clubs");
+        } else {
+            System.out.println("No matching tens of hearts were found");
+        }
+
+        System.out.println("Tens of club cards = " + Collections.frequency(deck, tenOfClubs));
+
+        System.out.println("Best card = " + Collections.max(deck, sortingAlgorithm));
+        System.out.println("Worst card = " + Collections.min(deck, sortingAlgorithm));
+
+        var sortBySuit = Comparator.comparing(Card198::suit).thenComparing(Card198::rank);
+        deck.sort(sortBySuit);
+        Card198.printDeck(deck, "Sorted by suit", 4);
+
+        List<Card198> copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.rotate(copied, 2);
+        System.out.println("UnRotate: " + deck.subList(0, 13));
+        System.out.println("Rotate: " + 2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.rotate(copied, -2);
+        System.out.println("UnRotate: " + deck.subList(0, 13));
+        System.out.println("Rotate: " + -2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        for (int i = 0; i < copied.size() / 2; i++) {
+            Collections.swap(copied, i, copied.size() - 1 - i);
+        }
+        System.out.println("Manual reverse :" + copied);
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.reverse(copied);
+        System.out.println("Using reverse :" + copied);
     }
 }
