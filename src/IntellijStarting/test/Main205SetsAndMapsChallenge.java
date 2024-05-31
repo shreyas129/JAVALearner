6d666faccb8e4d2de0f1b11a923217506ee7449d
Shreyas129 <shreyas.tathode129@gmail.com>
2024-05-31 12:27:47 +0530

    commit

--- src/IntellijStarting/test/Main205SetsAndMapsChallenge.java
+++ src/IntellijStarting/test/Main205SetsAndMapsChallenge.java
@@ -3,7 +3,7 @@ package IntellijStarting.test;
 import java.util.Collection;
 import java.util.List;
 
-public class Main205SetsAndMaps {
+public class Main205SetsAndMapsChallenge {
     public static void main(String[] args) {
         List<Contact205> emails = ContactData205.getData("email");
         List<Contact205> phones = ContactData205.getData("phone");
