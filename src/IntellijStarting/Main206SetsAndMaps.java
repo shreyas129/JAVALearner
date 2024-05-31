cd38a40171a88fba0646c731a5b2652f8fd927ff
Shreyas129 <shreyas.tathode129@gmail.com>
2024-05-31 12:23:16 +0530

    Collections Sets and HashSets Contacts Part 2

--- src/IntellijStarting/Main206SetsAndMaps.java
+++ src/IntellijStarting/Main206SetsAndMaps.java
@@ -24,9 +24,37 @@ public class Main206SetsAndMaps {
         robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.org");
         System.out.println(robinHood);
 
+        Set<Contact206> unionAB = new HashSet<>();
+        unionAB.addAll(emailContacts);
+        unionAB.addAll(phoneContacts);
+        printData("(A ∪ B) Union of emails (A) with phones (B)", unionAB);
+
+        Set<Contact206> intersectAB = new HashSet<>(emailContacts);
+        intersectAB.retainAll(phoneContacts);
+        printData("(A ∩ B) Intersect emails (A) with phones (B)", intersectAB);
+
+        Set<Contact206> intersectBA = new HashSet<>(phoneContacts);
+        intersectBA.retainAll(emailContacts);
+        printData("(B ∩ A) Intersect phones (B) with emails (A)", intersectBA);
+
+        Set<Contact206> AMinusB = new HashSet<>(emailContacts);
+        AMinusB.removeAll(phoneContacts);
+        printData("(A - B) emails (A) - phones (B)", AMinusB);
+
+        Set<Contact206> BMinusA = new HashSet<>(phoneContacts);
+        BMinusA.removeAll(emailContacts);
+        printData("(B - A) phones (B) - emails (A)", BMinusA);
+
+        Set<Contact206> symmetricDiff = new HashSet<>(AMinusB);
+        symmetricDiff.addAll(BMinusA);
+        printData("Symmetric Difference: phones and emails", symmetricDiff);
+
+        Set<Contact206> symmetricDiff2 = new HashSet<>(unionAB);
+        symmetricDiff2.removeAll(intersectAB);
+        printData("Symmetric Difference: phones and emails", symmetricDiff2);
     }
 
-        public static void printData(String header, Collection<Contact206> contacts) {
+    public static void printData(String header, Collection<Contact206> contacts) {
         System.out.println("---------------------------------------------");
         System.out.println(header);
         System.out.println("---------------------------------------------");
