7cfb9eac42e93cf8435776f92a0488d332a7d0fe
Shreyas129 <shreyas.tathode129@gmail.com>
2024-06-16 12:21:55 +0530

    Working with treeMap(NavigableMap) methods SortedMaps

--- src/IntellijStarting/Main219SortedMaps.java
+++ src/IntellijStarting/Main219SortedMaps.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class Main219SortedMaps {
-    private static Map<String, Purchase219> purchases = new LinkedHashMap<>();
-    private static NavigableMap<String, Student219> students = new TreeMap<>();
+    private static final Map<String, Purchase219> purchases = new LinkedHashMap<>();
+    private static final NavigableMap<String, Student219> students = new TreeMap<>();
 
     public static void main(String[] args) {
         Course219 jmc = new Course219("jmc101", "Java Master Class", "Java");
@@ -37,6 +37,54 @@ public class Main219SortedMaps {
             });
         }
         datedPurchases.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        int currentYear = LocalDate.now().getYear();
+        LocalDate firstDay = LocalDate.ofYearDay(currentYear, 1);
+        LocalDate week1 = firstDay.plusDays(7);
+        Map<LocalDate, List<Purchase219>> week1Purchases = datedPurchases.headMap(week1);
+        Map<LocalDate, List<Purchase219>> week2Purchases = datedPurchases.tailMap(week1);
+
+//        System.out.println("-----------------------");
+//        week1Purchases.forEach((key, value) -> System.out.println(key + ": " + value));
+//        System.out.println("-----------------------");
+//        week2Purchases.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        displayStats(1, week1Purchases);
+        displayStats(2, week2Purchases);
+
+        System.out.println("------------------------");
+
+        LocalDate lastDate = datedPurchases.lastKey();
+        var previousEntry = datedPurchases.lastEntry();
+
+        while (previousEntry != null) {
+            List<Purchase219> lastDaysData = previousEntry.getValue();
+            System.out.println(lastDate + " purchases : " + lastDaysData.size());
+
+            LocalDate prevDate = datedPurchases.lowerKey(lastDate);
+            previousEntry = datedPurchases.lowerEntry(lastDate);
+            lastDate = prevDate;
+        }
+
+        System.out.println("-----------------------");
+
+        var reversed = datedPurchases.descendingMap();
+        LocalDate firstDate = reversed.firstKey();
+//        var nextEntry = reversed.firstEntry();
+        var nextEntry = reversed.pollFirstEntry();
+        while (nextEntry != null) {
+            List<Purchase219> lastDaysData = nextEntry.getValue();
+            System.out.println(firstDate + " purchases : " + lastDaysData.size());
+
+            LocalDate nextDate = reversed.higherKey(firstDate);
+//            nextEntry = reversed.higherEntry(firstDate);
+            nextEntry = reversed.pollFirstEntry();
+            firstDate = nextDate;
+        }
+
+        System.out.println("-----------------------");
+
+        datedPurchases.forEach((key, value) -> System.out.println(key + ": " + value));
     }
 
     private static void addPurchase(String name, Course219 course, double price) {
@@ -48,10 +96,23 @@ public class Main219SortedMaps {
             existingStudent.addCourse(course);
         }
 
-        int day = new Random().nextInt(1, 115);
+        int day = new Random().nextInt(1, 15);
         String key = course.courseId() + "_" + existingStudent.getId();
         int year = LocalDate.now().getYear();
         Purchase219 purchase = new Purchase219(course.courseId(), existingStudent.getId(), price, year, day);
         purchases.put(key, purchase);
+    }
+
+    private static void displayStats(Integer period, Map<LocalDate, List<Purchase219>> periodData) {
+        System.out.println("----------------------");
+        Map<String, Integer> weeklyCounts = new TreeMap<>();
+        periodData.forEach((key, value) -> {
+            System.out.println(key + ": " + value);
+            for (Purchase219 p : value) {
+                weeklyCounts.merge(p.courseId(), 1, Integer::sum);
+            }
+        });
+
+        System.out.printf("Week %d purchases = %s%n", period, weeklyCounts);
     }
 }
